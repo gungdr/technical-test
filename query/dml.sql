f83aadf98a34e76085f32540e0ae5e8438007838
gungdr <775209+gungdr@users.noreply.github.com>
2020-11-27 21:43:07 +0700

    feat(query) : provide dml & ddl

---
+++ query/dml.sql
@@ -0,0 +1,6 @@
+insert into `user` (ID,UserName,Parent) 
+values (1,'Ali',2);
+insert into `user` (ID,UserName,Parent) 
+values (2,'Budi',0);
+insert into `user` (ID,UserName,Parent) 
+values (3,'Cecep',1);
