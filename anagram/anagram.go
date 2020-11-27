383b9d0af80bd037b539bcc1914d2a7a0cdacc9a
gungdr <775209+gungdr@users.noreply.github.com>
2020-11-27 22:32:33 +0700

    feat(anagram): group anagram words

---
+++ anagram/anagram.go
@@ -0,0 +1,37 @@
+package anagram
+
+import (
+	"fmt"
+)
+
+func anagramGroup(words []string) [][]string {
+	result := [][]string{}
+	wordMap := map[int64][]string{}
+	for _, word := range words {
+		val := int64(1)
+		for _, char := range word {
+			val *= int64(char)
+		}
+		wordMap[val] = append(wordMap[val], word)
+	}
+
+	for _, val := range wordMap {
+		result = append(result, val)
+	}
+	return result
+}
+
+func Run() {
+	// ['kita', 'atik', 'tika', 'aku', 'kia', 'makan', 'kua']
+	words := []string{
+		"kita",
+		"atik",
+		"tika",
+		"aku",
+		"kia",
+		"makan",
+		"kua",
+	}
+	result := anagramGroup(words)
+	fmt.Println(result)
+}
