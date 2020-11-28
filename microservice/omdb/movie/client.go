3023f6e6fd27abad5d4d2ce108ea5c251251484a
gungdr <775209+gungdr@users.noreply.github.com>
2020-11-29 00:18:05 +0700

    feat(microservice): add cache to client

--- microservice/omdb/movie/client.go
+++ microservice/omdb/movie/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"omdb/config"
 	"time"
@@ -19,6 +20,7 @@ type Client interface {
 type client struct {
 	resty  *resty.Client
 	config *config.Config
+	cache  map[string]string
 }
 
 func NewClient(conf *config.Config) Client {
@@ -30,19 +32,31 @@ func NewClient(conf *config.Config) Client {
 			SetTimeout(30 * time.Second).
 			SetRetryWaitTime(100 * time.Millisecond).
 			SetRetryMaxWaitTime(500 * time.Millisecond).
-			SetDebug(true),
+			SetDebug(false),
+		cache: make(map[string]string),
 	}
 }
 
 func (c *client) Search(ctx context.Context, param map[string]string) (*SearchResult, error) {
 	param["apikey"] = c.config.OMDBAPIKey
+	result := &SearchResult{}
+	cacheKey := getKey(param)
+	if cacheKey != "" {
+		if val, found := c.cache[cacheKey]; found {
+			err := json.Unmarshal([]byte(val), &result)
+			if err != nil {
+				return nil, err
+			}
+			return result, nil
+		}
+	}
 	response, err := c.resty.R().SetQueryParams(param).
 		Get("/")
 	if err != nil {
 		log.Println("Client Search:", err)
 		return nil, err
 	}
-	result := &SearchResult{}
+
 	err = json.Unmarshal([]byte(response.String()), &result)
 	if err != nil {
 		return nil, err
@@ -50,18 +64,29 @@ func (c *client) Search(ctx context.Context, param map[string]string) (*SearchRe
 	if result.Error != nil {
 		return nil, errors.New(*result.Error)
 	}
+	c.cache[cacheKey] = response.String()
 	return result, nil
 }
 
 func (c *client) Get(ctx context.Context, param map[string]string) (*MovieDetailResult, error) {
 	param["apikey"] = c.config.OMDBAPIKey
+	result := &MovieDetailResult{}
+	cacheKey := getKey(param)
+	if cacheKey != "" {
+		if val, found := c.cache[cacheKey]; found {
+			err := json.Unmarshal([]byte(val), &result)
+			if err != nil {
+				return nil, err
+			}
+			return result, nil
+		}
+	}
 	response, err := c.resty.R().SetQueryParams(param).
 		Get("/")
 	if err != nil {
 		log.Println("Client Get:", err)
 		return nil, err
 	}
-	result := &MovieDetailResult{}
 	err = json.Unmarshal([]byte(response.String()), &result)
 	if err != nil {
 		return nil, err
@@ -69,5 +94,17 @@ func (c *client) Get(ctx context.Context, param map[string]string) (*MovieDetail
 	if result.Error != nil {
 		return nil, errors.New(*result.Error)
 	}
+	c.cache[cacheKey] = response.String()
 	return result, nil
+}
+
+func getKey(param map[string]string) string {
+	if val, found := param["s"]; found {
+		page := param["page"]
+		return fmt.Sprintf("%s:%s", val, page)
+	}
+	if val, found := param["i"]; found {
+		return val
+	}
+	return ""
 }
