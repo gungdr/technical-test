6d7dceb913dbc37a467cd99318537cdf4faa557a
gungdr <775209+gungdr@users.noreply.github.com>
2020-11-29 14:20:58 +0700

    chore(query) : add sample data

--- query/dml.sql
+++ query/dml.sql
@@ -4,3 +4,11 @@ insert into `user` (ID,UserName,Parent)
 values (2,'Budi',0);
 insert into `user` (ID,UserName,Parent) 
 values (3,'Cecep',1);
+insert into `user` (ID,UserName,Parent) 
+values (4,'Paul',1);
+insert into `user` (ID,UserName,Parent) 
+values (5,'Parker',2);
+insert into `user` (ID,UserName,Parent) 
+values (6,'Sullivan',0);
+insert into `user` (ID,UserName,Parent) 
+values (7,'WillisSullivan',3);
